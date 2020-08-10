ae503ceb98ab0273db7ce96aa4e6d26440eebce3
Toni Margan <toni.margan@gmail.com>
2020-08-10 11:59:12 +0200

    added selection sort

--- Main.go
+++ Main.go
@@ -10,8 +10,12 @@ func main() {
 	var numOfElements int
 	fmt.Println("Enter number of elements in array: ")
 	fmt.Scanln(&numOfElements)
+
 	var array []int = createArray(numOfElements)
-	fmt.Println(array)
+	fmt.Println("-----------------------------")
+	fmt.Println("------- Array created -------")
+	fmt.Println("------ Sorting started ------")
+	selectionSort(array)
 }
 
 func createArray(numOfElements int) []int {
@@ -27,4 +31,24 @@ func shuffleArray(array []int) []int {
 	rand.Seed(time.Now().UnixNano())
 	rand.Shuffle(len(array), func(i, j int) { array[i], array[j] = array[j], array[i] })
 	return array
+}
+
+func selectionSort(array []int) {
+	var sortedArray []int = array
+	var length = len(sortedArray)
+	var startTime = time.Now()
+	for i := 0; i < length-1; i++ {
+		var min int = i
+		for j := i + 1; j < length; j++ {
+			if sortedArray[j] < sortedArray[min] {
+				min = j
+			}
+		}
+		sortedArray[i], sortedArray[min] = sortedArray[min], sortedArray[i]
+	}
+	var elapsed = time.Since(startTime)
+	fmt.Println("-----------------------------")
+	fmt.Println("Selection sort")
+	fmt.Println("Time elapsed:", elapsed.Nanoseconds(), "ns")
+	fmt.Println("-----------------------------")
 }
